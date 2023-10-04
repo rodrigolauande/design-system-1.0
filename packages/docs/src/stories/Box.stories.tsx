dcf949c1e5363d7301cd1d5464c61b0610ce5cf4
RODRIGO HENRIQUE LAUANDE <guigo0502@hotmail.com>
2023-10-04 12:59:37 -0300

    atualização

---
+++ packages/docs/src/stories/Box.stories.tsx
@@ -0,0 +1,23 @@
+import { Meta, StoryObj } from '@storybook/react'
+import { Box, BoxProps, Text } from '@ignite-ui/react'
+
+export default {
+  title: 'Surface/Box',
+  component: Box,
+  args: {
+    children: (
+      <>
+        <Text>Testando o elemento</Text>
+      </>
+    ),
+  },
+  argTypes: {
+    children: {
+      control: {
+        type: null,
+      },
+    },
+  },
+} as Meta<BoxProps>
+
+export const Primary: StoryObj<BoxProps> = {}
