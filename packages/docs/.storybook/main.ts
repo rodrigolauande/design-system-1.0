681f7f19e104c92119d18bded31370bc1209da51
RODRIGO HENRIQUE LAUANDE <guigo0502@hotmail.com>
2023-10-05 11:40:00 -0300

    fix: Add subpath to Storybook on production

--- packages/docs/.storybook/main.ts
+++ packages/docs/.storybook/main.ts
@@ -29,7 +29,7 @@ const config: StorybookConfig = {
   },
   viteFinal: (config, { configType }) => {
     if (configType === 'PRODUCTION') {
-      config.base = '/05-design-system/'
+      config.base = '/05-DESIGN-SYSTEM/'
     }
 
     return config
